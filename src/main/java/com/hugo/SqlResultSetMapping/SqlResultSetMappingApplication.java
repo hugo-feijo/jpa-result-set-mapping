60481b2be4a246134765fef658218a731414d192
hugo.oliveira <hugo.oliveira@evoluum.com.br>
2021-02-17 13:49:54 -0300

    Commit inicial

---
+++ src/main/java/com/hugo/SqlResultSetMapping/SqlResultSetMappingApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.SqlResultSetMapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlResultSetMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlResultSetMappingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugo/SqlResultSetMapping/SqlResultSetMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.SqlResultSetMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlResultSetMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
